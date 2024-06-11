0b9f206f27425c8c38b50c3eeb275d527cffb826
Manthan Sharma <mayank9870866130@gmail.com>
2024-06-11 13:28:33 +0530

    add configuration setup

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
 
 const startServer = () => {
   try {
-    const PORT = process.env.PORT || 3000;
+    const PORT = config.port || 3000;
 
     app.listen(PORT, () => {
       console.log(`Server listening on PORT=${PORT}`);
