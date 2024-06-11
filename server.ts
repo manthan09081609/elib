69e4f30e3bde7af380b66d73b0aa62f74d12f8b7
Manthan Sharma <mayank9870866130@gmail.com>
2024-06-11 14:04:05 +0530

    add database setup

--- server.ts
+++ server.ts
@@ -1,16 +1,19 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
+import connectDB from "./src/config/db";
 
-const startServer = () => {
+const startServer = async () => {
   try {
+    await connectDB();
     const PORT = config.port || 3000;
 
     app.listen(PORT, () => {
       console.log(`Server listening on PORT=${PORT}`);
     });
   } catch (err) {
-    console.log(err);
+    console.log("some error occured", err);
+    process.exit(1);
   }
 };
 
-startServer();
+void startServer();
